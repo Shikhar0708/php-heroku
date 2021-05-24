101b851977163cb85cd5b81dc47d837517415f59
Shikhar Sinha <50794248+Shikhar0708@users.noreply.github.com>
2021-05-24 22:44:43 +0530

    composer.json file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//Creates new record as per request
+    //Connect to database
+    $servername = "dot.cil0lbnaent8.ap-south-1.rds.amazonaws.com";
+    $username = "admin";
+    $password = "dotnrdgmail";
+    $dbname = "dot";
+    ?>
